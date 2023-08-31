eb48df23d46050394e16e2ced9a6ca18c5b2d35c
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-08-31 12:31:11 +0100

    bump shell version to make use of the proxy mechanism

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const config = require('@rancher/shell/vue.config');
+const configHelper = require('@rancher/shell/vue-config-helper.js');
 
 module.exports = config(__dirname, {
   excludes: [],
-  // excludes: ['fleet', 'example']
+  proxies:  { '/rhn/manager': configHelper.proxyOpts('https://ec2-52-206-103-214.compute-1.amazonaws.com') }
 });
